53f8e7386105b55a888e9df779605c4db2d387ea
luan santos <lu37321695@gmail.com>
2023-10-03 12:19:54 -0300

    learning_about_URLs

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+
+def my_view(request):
+    return HttpResponse("UMA LINDA STRING")
+
+
+urlpatterns = [path("admin/", admin.site.urls), path("sobre/", my_view)]
